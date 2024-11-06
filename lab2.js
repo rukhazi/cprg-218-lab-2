d2e2c8d47d5d775afdea313ca6ea646b315e6d86
andrea <ramora.drea@gmail.com>
2024-11-06 16:49:36 -0700

    please save

---
+++ lab2.js
@@ -0,0 +1,24 @@
+sheepbutton.addEventListener("click",function(){
+    console.log("sheepclicked");
+    document.getElementById("image").src="sheep.webp";
+})
+
+bearbutton.addEventListener("click",function(){
+    console.log("bearclicked");
+    document.getElementById("image").src="bear.webp";
+})
+
+moosebutton.addEventListener("click",function(){
+    console.log("mooseclicked");
+    document.getElementById("image").src="moose.webp";
+})
+
+wolfbutton.addEventListener("click",function(){
+    console.log("wolfclicked");
+    document.getElementById("image").src="wolf.webp";
+})
+
+elkbutton.addEventListener("click",function(){
+    console.log("elkclicked");
+    document.getElementById("image").src="elk.webp"
+})
